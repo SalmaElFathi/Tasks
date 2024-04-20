97a44d7eac0ca74eebad0ecc33f89fa69f581a90
salma el fathi <fathisalma374@gmail.com>
2024-04-20 17:19:04 +0100

    renamed:    building front-end applications with react/task23/ravenous/.babelrc -> building front-end applications with react/task23_25_27_28/ravenous/.babelrc

--- building front-end applications with react/task23_25_27_28/ravenous/src/index.js
+++ building front-end applications with react/task23_25_27_28/ravenous/src/index.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
-import unregister from './registerServiceWorker.js';
+import register from './registerServiceWorker.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
+register();
 
 
